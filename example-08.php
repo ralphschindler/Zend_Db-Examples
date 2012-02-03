e37ca71357983539397126000d53e8fa06e7438a
Ralph Schindler <ralphschindler@yahoo.com>
2012-02-03 14:55:41 -0600

    Examples for insert(), update(), & delete() for TableGateway updated.

--- example-08.php
+++ example-08.php
@@ -1,5 +1,17 @@
 <?php
 
-/**
- * Issue SELECT command through adapter using paramaterization, iterating result as array values
- */
+$adapter = include ((file_exists('bootstrap.php')) ? 'bootstrap.php' : 'bootstrap.dist.php');
+refresh_data($adapter);
+
+/* @var $adapter Zend\Db\Adapter */
+$artistTable = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
+$result = $artistTable->update(array('name' => 'New Artist'), array('id' => 2));
+
+assert_example_works($result === 1, true);
+
+$artistTable = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
+$rowset = $artistTable->select(array('id' => 2));
+$row = $rowset->current();
+
+$name = $row['name'];
+assert_example_works($name == 'New Artist');
--- example-09.php
+++ example-09.php
@@ -1,5 +1,16 @@
 <?php
 
-/**
- * Issue SELECT command through adapter using paramaterization, iterating result as objects
- */
+$adapter = include ((file_exists('bootstrap.php')) ? 'bootstrap.php' : 'bootstrap.dist.php');
+refresh_data($adapter);
+
+/* @var $adapter Zend\Db\Adapter */
+$artistTable = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
+$result = $artistTable->delete(array('id' => 2));
+
+assert_example_works($result === 1, true);
+
+$artistTable = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
+$rowset = $artistTable->select(array('id' => 2));
+$row = $rowset->current();
+
+assert_example_works($row == false);
