836f8fc129149a9a62765a8c693e6f2198506e8b
Ralph Schindler <ralphschindler@yahoo.com>
2012-02-29 00:01:57 -0600

    Updated example-13

--- example-13.php
+++ example-13.php
@@ -8,23 +8,11 @@
 $adapter = include ((file_exists('bootstrap.php')) ? 'bootstrap.php' : 'bootstrap.dist.php');
 refresh_data($adapter);
 
-//$table = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
-
 $where = new Zend\Db\Sql\Where();
-
 $where->equalTo('id', 1)->OR->equalTo('id', 2);
 $where->nest('OR')->like('name', 'Ralph%')->OR->greaterThanOrEqualTo('age', 30)->AND->lessThanOrEqualTo('age', 50);
 $where->literal('foo = ?', 'bar');
 
+$target = ' WHERE "id" = \'1\' OR "id" = \'2\' OR ("name" LIKE \'Ralph%\' OR "age" >= \'30\' AND "age" <= \'50\') AND foo = \'bar\'';
 
-$stmt = $adapter->createStatement();
-$stmt->setSql('SELECT * FROM artist');
-$where->prepareStatement($adapter, $stmt);
-var_dump($stmt->getSql(), $stmt->getParameterContainer());
-die();
-
-$artists = $table->select($where);
-var_dump($artists);
-foreach ($artists as $artist) {
-    //var_dump($artist->toArray());
-}
+assert_example_works($target == $where->getSqlString());
