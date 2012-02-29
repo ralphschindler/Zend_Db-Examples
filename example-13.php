f81e25f585523733631f9d1cbaf7fd572ff55487
Ralph Schindler <ralphschindler@yahoo.com>
2012-02-28 22:12:15 -0600

    Updated 13

--- example-13.php
+++ example-13.php
@@ -11,9 +11,12 @@ refresh_data($adapter);
 //$table = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
 
 $where = new Zend\Db\Sql\Where();
+
 $where->equalTo('id', 1)->OR->equalTo('id', 2);
-$where->nest('OR')->like('name', 'Ralph%')->OR->greaterThanOrEqualTo('age', 30);
+$where->nest('OR')->like('name', 'Ralph%')->OR->greaterThanOrEqualTo('age', 30)->AND->lessThanOrEqualTo('age', 50);
 $where->literal('foo = ?', 'bar');
+
+
 $stmt = $adapter->createStatement();
 $stmt->setSql('SELECT * FROM artist');
 $where->prepareStatement($adapter, $stmt);
@@ -25,5 +28,3 @@ var_dump($artists);
 foreach ($artists as $artist) {
     //var_dump($artist->toArray());
 }
-
-//$where->nest('OR')->like('name', 'Ralph%')->OR->greaterThanOrEqualTo('age', 30)->OR;
