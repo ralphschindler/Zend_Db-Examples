f4f8a4a14616f540137cee3dd0ff8aa70151bfd4
Ralph Schindler <ralphschindler@yahoo.com>
2012-02-28 21:12:12 -0600

    Working on example 13, Where

--- example-13.php
+++ example-13.php
@@ -4,10 +4,26 @@
  * Issue SELECT command through adapter using name based container paramaterization
  */
 
+/** @var $adapter Zend\Db\Adapter\Adapter */
 $adapter = include ((file_exists('bootstrap.php')) ? 'bootstrap.php' : 'bootstrap.dist.php');
 refresh_data($adapter);
 
-$sql = new Zend\Db\Sql\Select;
-$sql->from('foo');
-$sql->where(array('id > ?' => 2));
-echo $sql->getSqlString($adapter->getPlatform());
+//$table = new Zend\Db\TableGateway\TableGateway('artist', $adapter);
+
+$where = new Zend\Db\Sql\Where();
+$where->equalTo('id', 1)->OR->equalTo('id', 2);
+$where->nest('OR')->like('name', 'Ralph%')->OR->greaterThanOrEqualTo('age', 30);
+$where->literal('foo = ?', 'bar');
+$stmt = $adapter->createStatement();
+$stmt->setSql('SELECT * FROM artist');
+$where->prepareStatement($adapter, $stmt);
+var_dump($stmt->getSql(), $stmt->getParameterContainer());
+die();
+
+$artists = $table->select($where);
+var_dump($artists);
+foreach ($artists as $artist) {
+    //var_dump($artist->toArray());
+}
+
+//$where->nest('OR')->like('name', 'Ralph%')->OR->greaterThanOrEqualTo('age', 30)->OR;
