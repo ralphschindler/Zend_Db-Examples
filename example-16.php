c7978d53fd9c38461b2d67bcffdb1fc4b8507531
Ralph Schindler <ralphschindler@yahoo.com>
2012-05-20 18:42:48 -0500

    Removed debugging code

--- example-16.php
+++ example-16.php
@@ -21,7 +21,6 @@ $select->prepareStatement($adapter, $statement);
 $resultSet = new ResultSet();
 
 $container = $statement->getParameterContainer();
-//var_dump($statement);
 
 // as we iterate bind the new offset to the existing statement
 foreach (array(0, 2, 4) as $offset) {
@@ -33,7 +32,7 @@ foreach (array(0, 2, 4) as $offset) {
     foreach ($resultSet->toArray() as $row) {
         $output .= '|' . $row['title'] . '|';
     }
-//var_dump($output);
+
     switch ($offset) {
         case 0:
             assert_example_works($output === '|...Baby One More Time||Oops!... I Did It Again|', true);
